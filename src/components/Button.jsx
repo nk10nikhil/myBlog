9aea4c196db64503a35ed317e31c37df312c6b52
Nikhil Kumar <nk10nikhil@gmail.com>
2025-02-06 04:53:17 +0530

    building react components

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Button({
+    children,
+    type = "button",
+    bgColor = "bg-blue-600",
+    textColor = "text-white",
+    className = "",
+    ...props
+}) {
+    return (
+        <button className={`px-6 py-2 duration-200 rounded-full ${bgColor} ${textColor} ${className}`} {...props}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
---
+++ src/components/container/container.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Container(children) {
+    return (
+        <div className="w-full max-w-4xl mx-auto px-4">
+        {children}
+        </div>
+    );
+
+}
+
+export default Container;
