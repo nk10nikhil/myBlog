aa3dca957634c5a8cf3c4200e9d865058fdd17be
Nikhil Kumar <nk10nikhil@gmail.com>
2025-02-06 02:12:24 +0530

    config and appwrite setup

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+const config = {
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    // appwriteProjectID: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    // appwriteDatabaseID: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    // appwriteCollectionID: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    // appwriteBucketID: String(process.env.REACT_APP_APPWRITE_BUCKET_ID)
+}
+
+export default config;
