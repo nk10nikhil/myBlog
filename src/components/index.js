2264d8aa1522a74d889e4412f8f92f3142252690
Nikhil Kumar <nk10nikhil@gmail.com>
2025-02-08 03:52:57 +0530

    create RTE and add slug values

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,26 @@ import Footer from './Footer/Footer.jsx';
 import Container from './container/container.jsx';
 import Logo from '../logo.jsx';
 import LogoutBtn from './Header/LogoutBtn.jsx';
+import SignUp from './SignUp.jsx';
+import Select from './Select.jsx';
+import RTE from './RTE.jsx';
+import PostCard from './PostCard.jsx';
+import Login from './Login.jsx';
+import Input from './Input.jsx';
+import AuthLayout from './AuthLayout.jsx';
 
 export {
     Header,
     Footer,
+    Input,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Login,
+    SignUp,
+    Select,
+    RTE,
+    PostCard,
+    AuthLayout
+
 };
